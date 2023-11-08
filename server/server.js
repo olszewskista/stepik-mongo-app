98f9d8bfc85d56575fe3e927d74d5a115b76fc04
Stanislaw Olszewski <olszewski.sta@gmail.com>
2023-11-08 23:03:46 +0100

    lux2

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const db = require("./db/conn");
 const routes = require("./routes/record");
 
 app.use(express.json());
+app.use(cors())
 
 // Connect to the database
 db.connectToDatabase();

8a1afa756b634d0bef8806eec12d9b6969fbc5e9
Stanislaw Olszewski <olszewski.sta@gmail.com>
2023-11-13 18:07:48 +0100

    server done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
+// require("dotenv").config();
 const port = process.env.PORT || 3000;
 const db = require("./db/conn");
 const routes = require("./routes/record");
@@ -9,13 +9,10 @@ const routes = require("./routes/record");
 app.use(express.json());
 app.use(cors())
 
-// Connect to the database
 db.connectToDatabase();
 
-// Use the routes
 app.use("/stepik", routes);
 
-// Start the Express.js server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
